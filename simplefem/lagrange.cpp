3084125d0ddf9d7c24a7399432c02fac41017773
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-03-25 10:08:10 +0000

    degree 2

--- simplefem/lagrange.cpp
+++ simplefem/lagrange.cpp
@@ -21,6 +21,23 @@ double LagrangeElementTriangle::evaluate(int basis_function_index,
       default:
         throw std::runtime_error("Incorrect basis function index");
       }
+    case 2:
+      switch(basis_function_index) {
+        case 0:
+          return (1.0 - point[0] - point[1]) * (1.0 - 2 * point[0] - 2 * point[1]);
+        case 1:
+          return (1.0 - point[0] - point[1]) * 4 * point[0];
+        case 2:
+          return point[0] * (2 * point[0] - 1);
+        case 3:
+          return (1.0 - point[0] - point[1]) * 4 * point[1];
+        case 4:
+          return 4 * point[0] * point[1];
+        case 5:
+          return point[1] * (2 * point[1] - 1);
+      default:
+        throw std::runtime_error("Incorrect basis function index");
+      }
     default:
       throw std::runtime_error("Unsupported degree");
   }
